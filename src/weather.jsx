2329ae808da5926933953607bda0f2619b69b199
gedeno <gedenogeremew@gmail.com>
2026-06-15 10:59:05 +0300

    the air coditons and humdity are fixed

--- src/weather.jsx
+++ src/weather.jsx
@@ -3,10 +3,12 @@ import { useState ,useEffect } from "react"
 import "./weather.css"
 
 export const Weather_data = () => {
-    const [weather , setWeather] = useState({list:[]})
+    const [weather , setWeather] = useState({list:[{main:{temp:0}, wind:{speed:0}}]})
     const [city , setcity] = useState("adama")
     const [temps, setTemps] =  useState([])
     const [time ,setTime] = useState([])
+    const [humdity , sethumdity] = useState(0)
+    const [Wspeed , setWspeed] = useState(0)
     const ap = "e8dda8cf771d6292c8f62887f2ce38d5"
     const API = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${ap}&units=metric`
     
@@ -20,29 +22,39 @@ export const Weather_data = () => {
         }).catch((error)=>{
           console.log(error)
         })
-    },[])
+    },[city])
     const Cityinput = (e) => {
       setcity(e.target.value)
     }
-    const HumAir = () => {
-        const humdity = weather.list[0].main.feels_like
-        const WSpeed = weather.list[0].wind.speed
-        return ({humdity , WSpeed});
-        console.log(humdity)
-    }
+    
     const DayTemp = () => {
+      axios.get(API).then((response)=>{
+            setWeather(response.data)
+            setTemps(response.data.list.slice(0,8).map(item => item.main.temp));
+            setTime(response.data.list.slice(0,8).map(item => item.dt_txt.slice(11,16)))
+            setWspeed(response.data.list[0].main.wind.speed)
+        }).catch((error)=>{
+          console.log(error)
+        })
       const temps = weather.list.slice(0, 8).map((item) => item.main.temp)
       const time = weather.list.slice(0,8).map(item => item.dt_txt.slice(11,16))
+      const humdity = weather.list[0].main.feels_like
+      const WSpeed = weather.list[0].wind.speed
       setTemps(temps);
       setTime(time);
+      setWspeed(WSpeed)
+      sethumdity(humdity)
       console.log(temps);
+      console.log(humdity)
+      console.log(WSpeed)
+      
     };
-    
+
     return (
       <div className="ALL1">
         <div className="searchDiv">
-          <input className="Search" type="text" onChange={Cityinput} />
-          <button className="searchB" onClick={DayTemp}>
+          <input value={city} className="Search" type="text" onChange={Cityinput} />
+          <button className="searchB" onClick={DayTemp }>
             search
           </button>
         </div>
@@ -65,6 +77,12 @@ export const Weather_data = () => {
           <div className="Air_con">
             {" "}
             <h3>Air condition</h3>
+            <div className="air_con">
+                
+              <div className="air"><h3>humdity:{humdity}</h3></div>
+              <div className="air"><h3>wind speed:{Wspeed}</h3></div>
+        
+            </div>
           </div>
         </div>
         <div className="SevDAY">
